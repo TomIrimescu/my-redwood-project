347023f410f471038c98a6657dd3eff672286125
Tom Irimescu <tom@irimescu.com>
2023-06-25 14:41:48 -0400

    Generate jest test files

--- api/src/services/posts/posts.test.ts
+++ api/src/services/posts/posts.test.ts
@@ -1,3 +1,6 @@
+// thrown: "Exceeded timeout of 5000 ms for a hook.
+// Add a timeout value to this test to increase the timeout, if this is a long-running test. See https://jestjs.io/docs/api#testname-fn-timeout."
+
 import type { Post } from '@prisma/client'
 
 import { posts, post, createPost, updatePost, deletePost } from './posts'
